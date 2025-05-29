22384a7d6e726ac2789d485cb542f2d6ffb78804
ahn_minji <ahn_minji@naver.com>
2025-05-29 16:57:35 +0900

    feat: (YGI-13) main 코드 초기화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Next.js with Tailwind CSS!</h1>
-    </main>
+    <div>
+      <main></main>
+    </div>
   );
 }
