dc8ee2501bfc4c67299fa644a0995cc9be52d2de
alotofhee <ekgmljeong@gmail.com>
2025-06-02 03:01:54 +0900

    feat: [YGI-18] 모바일 레이아웃 적용을 위한 LayoutWrapper 컴포넌트 생성 및 RootLayout 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import "./globals.css";
+import LayoutWrapper from "@/components/common/LayoutWrapper";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased bg-white text-black">{children}</body>
+    <html lang="ko">
+      <body className="bg-white text-black antialiased flex justify-center">
+        <LayoutWrapper>{children}</LayoutWrapper>
+      </body>
     </html>
   );
 }
