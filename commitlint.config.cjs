b35d4b2f66ec1a7f7dd36e5edb661699be9cf8db
gichulLimitLess <hanascreen@konkuk.ac.kr>
2025-06-02 11:12:07 +0900

    #17 feat: Husky+Commitlint 및 Eslint+Prettier 재설정

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,13 +1,18 @@
-// commitlint.config.cjs
+// #[issue-number] [type]: [commit message] -> 해당 형식에 맞춘 커밋 메시지 작성을 강제하는 commitlint 설정값
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "header-pattern": [
+      2,
+      "always",
+      "^#\\d+\\s+(feat|fix|docs|style|refactor|test|chore):\\s.+$",
+    ],
     "type-enum": [
       2,
       "always",
       ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
     ],
-    "type-case": [2, "always", "lower-case"],
+    "type-case": [0], // type 대소문자 검사 비활성화 (직접 추출 어려움)
     "subject-empty": [2, "never"],
     "header-max-length": [2, "always", 120],
   },
