45a677b65f89c717720c7d6265caf1be100c498a
alotofhee <ekgmljeong@gmail.com>
2025-06-09 15:46:38 +0900

    #36 feat: chat text ver 마크업

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,23 +1,69 @@
 "use client";
 
-import CharacterScene from "@/components/chat/CharacterScene";
-import Header from "@/components/common/Header";
+import { useEffect, useRef, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Message } from "@/types/Message";
+import Header from "@/components/common/Header";
+import TextPage from "./TextPage";
+
 type Mode = "text" | "voice";
 
-export default function ChatbotPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
+const dummyMessages: Message[] = [
+  {
+    role: "bot",
+    content:
+      "너 요즘 휴대폰으로 데이터 많이 쓰는 거 자주 해? 예를 들면, 휴대폰으로 영상을 많이 본다던가, 게임을 많이 한다던가 말이야.",
+  },
+  { role: "user", content: "난 정말 집에 가고 싶은데 말이야." },
+  {
+    role: "bot",
+    content:
+      "너 요즘 휴대폰으로 데이터 많이 쓰는 거 자주 해? 예를 들면, 휴대폰으로 영상을 많이 본다던가, 게임을 많이 한다던가 말이야.",
+  },
+  { role: "user", content: "나 휴대폰으로 영상 정말 많이 봐." },
+];
 
-  // 현재 모드 추출
+export default function ChatbotPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const mode = (searchParams.get("mode") as Mode) || "text";
+  const isVoiceMode = mode === "voice";
+
+  const [messages, setMessages] = useState<Message[]>(dummyMessages);
+  const [input, setInput] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    setMessages((prev) => [...prev, { role: "user", content: input }]);
+    setInput(""); // 초기화
+  };
+
+  // 스크롤 아래로 이동
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  // 입력창 자동 높이 조절
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [input]);
 
   return (
     <div className="flex h-screen flex-col bg-pink-100">
       <Header />
-      {/* 내용 영역 */}
-      <div className="flex-1 overflow-hidden">
-        {mode === "text" ? <h1>text</h1> : <h1>voice</h1>}
+      <div className="flex flex-1 flex-col overflow-hidden">
+        {mode === "text" ? (
+          <TextPage messages={messages} setMessages={setMessages} />
+        ) : (
+          <h1>voice</h1>
+        )}
       </div>
     </div>
   );
