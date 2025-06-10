73004cc10f59b131900320800e45843b84c9b3ee
박희준 <isk8520@naver.com>
2025-06-10 17:36:21 +0900

    feat(chart): 상단 UI 수정

--- src/app/tendency/[id]/page.tsx
+++ src/app/tendency/[id]/page.tsx
@@ -3,7 +3,7 @@
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import TendencyRadarChart from '@/components/chart/TendencyRadarChart';
-import { ArrowLeft } from 'lucide-react';
+import { ChevronLeft } from 'lucide-react';
 
 export default function PlanDetailPage() {
   const { id } = useParams();
@@ -35,27 +35,29 @@ export default function PlanDetailPage() {
     return <div className="p-4 text-gray-500"> 데이터 로딩 중...</div>;
 
   return (
-    <main className="flex flex-col items-center px-4 py-8 space-y-8 bg-[#fff6d8] min-h-screen">
-      
+    <main className="flex flex-col items-center space-y-8 bg-[#fff6d8] min-h-screen">
+
       <section className="w-full max-w-md">
         
-        <div className="flex items-center gap-2 mb-3 bg-[#ffffff]">
-          <ArrowLeft size={20} />
+        <div className="w-full flex items-center gap-23 mb-3 bg-[#ffffff]">
+          <div className="flex items-center px-4">
+          <ChevronLeft className="h-10 w-6" />
+        </div>
           <span className="text-sm font-semibold">요금제 상세 정보</span>
         </div>
 
-        
-        <h1 className="text-[24px] font-bold text-gray-900 leading-tight">
+      <div className="px-8">
+        <h1 className="text-[28px] font-bold text-gray-900 leading-tight mt-[20px]">
           {planName}
         </h1>
-        <p className="text-[20px] font-semibold text-gray-900">{price}</p>
+        <p className="text-[17px] font-semibold text-gray-900">{price}</p>
 
         
-        <div className="flex flex-wrap gap-2 my-3">
+        <div className="flex flex-wrap gap-1 my-1 mb-[20px]">
           {tags.map((tag, index) => (
             <span
               key={index}
-              className={`px-3 py-1 rounded-full text-xs font-medium
+              className={`px-1 py-1 rounded-md text-[10px] font-medium
                 ${index === 0 ? 'bg-red-100 text-red-500'
                 : index === 1 ? 'bg-indigo-100 text-indigo-500'
                 : 'bg-gray-100 text-gray-600'}`}
@@ -66,15 +68,14 @@ export default function PlanDetailPage() {
         </div>
 
         
-        <div className="flex space-x-2 bg-gray-400 p-1 rounded-full shadow-sm">
-          <button className="flex-1 py-1 rounded-full bg-white text-black font-semibold text-sm">
+        <div className="flex space-x-2 bg-gray-400 p-1 rounded-lg shadow-sm mb-4">
+          <button className="flex-1 py-1 rounded-lg bg-white text-black font-semibold text-sm">
             기본
           </button>
-          <button className="flex-1 py-1 rounded-full text-black text-sm hover:text-gray-700">
+          <button className="flex-1 py-1 rounded-lg text-black text-sm hover:text-gray-700">
             내 요금제와 비교
           </button>
         </div>
-      </section>
 
       
       <div className="w-full max-w-md space-y-8">
@@ -88,7 +89,12 @@ export default function PlanDetailPage() {
         <div className="w-full bg-white rounded-xl shadow p-4">
           <TendencyBarChart data={barData} name={planName} />
         </div>
+
+        </div>
+        
       </div>
+      </section>
+        
     </main>
   );
 }
