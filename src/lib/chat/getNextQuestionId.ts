73957592839ca94c8b434358368315c719a7d55d
gichulLimitLess <hanascreen@konkuk.ac.kr>
2025-06-12 01:20:04 +0900

    #52 fix: 구독 서비스 관련 질문 및 프롬프트 추가

--- src/lib/chat/getNextQuestionId.ts
+++ src/lib/chat/getNextQuestionId.ts
@@ -6,9 +6,12 @@ export function getNextQuestionId(
   questionId: number,
   normalized: string
 ): number | undefined {
+  const flow = questionFlow[questionId];
+
+  // 수치형 답변인 경우
   if (/^[0-9]+$/.test(normalized)) {
-    // 수치형 답변일 경우
-    return questionFlow[questionId]["__NUMERIC__"];
+    return flow["__NUMERIC__"] ?? flow["INVALID"];
   }
-  return questionFlow[questionId]?.[normalized];
+
+  return flow[normalized] ?? flow["__DEFAULT__"]; // 12번 질문에 대해서 이것이 유용히 사용될 것임
 }
