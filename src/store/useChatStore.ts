dcdca5d1b2a790fc422ac3f3d585fa5e041e3411
silverain <ekdldkaa@naver.com>
2025-06-11 10:39:25 +0900

    #63 feat: messages 전역상태로 persist관리

--- src/store/useChatStore.ts
+++ src/store/useChatStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { Message } from "@/types/Message";
 import { questionTextMap } from "@/lib/chat/chatBotQuestionFlow";
 
@@ -13,10 +14,18 @@ interface ChatStore {
   clearMessages: () => void;
 }
 
-export const useChatStore = create<ChatStore>((set) => ({
-  messages: initialMessage,
-  setMessages: (messages) => set({ messages }),
-  appendMessage: (message) =>
-    set((state) => ({ messages: [...state.messages, message] })),
-  clearMessages: () => set({ messages: initialMessage }),
-}));
+export const useChatStore = create<ChatStore>()(
+  persist(
+    (set) => ({
+      messages: initialMessage,
+      setMessages: (messages) => set({ messages }),
+      appendMessage: (message) =>
+        set((state) => ({ messages: [...state.messages, message] })),
+      clearMessages: () => set({ messages: initialMessage }),
+    }),
+    {
+      name: "chat-storage", // localStorage key 이름
+      partialize: (state) => ({ messages: state.messages }), // 저장할 필드 제한
+    }
+  )
+);
