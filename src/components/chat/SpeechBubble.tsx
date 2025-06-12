31b9e8786a96bab94242fc16e1e2a11069d3d4c3
silverain02 <108103346+silverain02@users.noreply.github.com>
2025-06-12 15:40:33 +0900

    #81 chore: 주석삭제

--- src/components/chat/SpeechBubble.tsx
+++ src/components/chat/SpeechBubble.tsx
@@ -1,4 +1,3 @@
-// components/SpeechBubble.tsx
 export default function SpeechBubble({ text }: { text: string }) {
   return (
     <div className="absolute top-[15%] w-fit max-w-[80%] rounded-2xl border bg-white px-4 py-3 text-center text-base font-medium text-gray-800 shadow-md">
