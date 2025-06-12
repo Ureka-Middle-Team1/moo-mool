9ec3b060fc2454ae3ab94a31a881ff16e89eab7e
silverain <ekdldkaa@naver.com>
2025-06-12 15:33:07 +0900

    #77 feat: 요금제 상세 db연동 및 어뎁터 패턴 적용

--- src/app/plandetail/[id]/page.tsx
+++ src/app/plandetail/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useGetPlanDetailQuery } from "@/hooks/useGetPlanDetailQuery";
 import {
   PlanInfo,
   PlanCharts,
@@ -11,9 +10,18 @@ import {
   BottomGradient,
 } from "@/components/planDetail";
 import { PlanDetailData } from "@/types/planDetail";
+import { useParams } from "next/navigation";
+import { useGetPlanById } from "@/hooks/useGetPlanById";
+import { mapPlanToDetailData } from "@/utils/mapPlanToDetailData";
 
 export default function PlanDetailPage() {
-  const { data, isLoading, isError, error } = useGetPlanDetailQuery();
+  const params = useParams();
+  const id = Number(params.id);
+
+  const { data, isLoading, isError, error } = useGetPlanById(
+    isNaN(id) ? undefined : id
+  );
+
   const [mode, setMode] = useState<"basic" | "compare">("basic");
   const [showGradient, setShowGradient] = useState(false);
 
@@ -26,7 +34,7 @@ export default function PlanDetailPage() {
   if (isError) return <div className="p-4 text-red-500">{error.message}</div>;
   if (!data) return <div className="p-4 text-gray-500">데이터 없음</div>;
 
-  const planData: PlanDetailData = data;
+  const planData: PlanDetailData = mapPlanToDetailData(data);
 
   return (
     <main className="relative flex min-h-screen flex-col items-center space-y-8 overflow-hidden bg-gradient-to-b from-[#fff6d8] to-white">
