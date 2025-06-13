276f5356e4b9fdd32b26186e71e3ae9707d69031
arty0928 <arty0928@konkuk.ac.kr>
2025-06-13 13:42:55 +0900

    #0 plan 코드 일단 받기

--- src/types/plan.ts
+++ src/types/plan.ts
@@ -1,7 +1,24 @@
-// 밈 테스트에서 사용하는 Plan
+// Smart Choice API 응답 시 받는 자료형
 export interface Plan {
-  rank: number;
-  title: string;
-  subtitle: string;
-  detail: string;
+  v_tel: string; // 이동통신사(SKT,KT,LGU+)
+  v_plan_price: string; // 예상 금액
+  v_dis_price: string; // 약정 할인 금액
+  v_plan_over: string; // 기본 제공량 초과 사용량과 금액
+  v_add_name: string; // 요금제 기본료, 부가서비스 기본료
+  v_plan_name: string; // 요금제명, 부가서비스명
+  v_plan_display_voice: string; // 음성 기본 제공량
+  v_display_data: string; // 데이터 기본 제공량
+  v_plan_sms: string; // 문자 기본 제공량
+  rn: string; // 요금제 추천 구분(으뜸: 1, 알뜰: 2, 넉넉: 3)
 }
+
+export type PlanSelectionInput = {
+  smartChoicePlans: Plan[];
+  subscribe: string;
+};
+
+export type PlanSelectionOutput = {
+  type: "SMART_CHOICE_ONLY" | "SUBSCRIBE_MATCH" | "FALLBACK";
+  smartChoicePlans?: Plan[]; // Smart Choice로 받아온 것은 없을 수도 있다
+  subscribe?: string; // 구독 서비스에 대해 받아온 정보 또한 없을 수도 있다
+};
