ebb8888fc3d1b2dca266d444386427eb6028a4ed
silverain <ekdldkaa@naver.com>
2025-06-14 16:25:21 +0900

    #112 style: 플로팅 버튼 UI 조정

--- src/components/common/FloatingChatbotButton.tsx
+++ src/components/common/FloatingChatbotButton.tsx
@@ -7,14 +7,16 @@ export default function FloatingChatbotButton() {
 
   return (
     <div
-      className="fixed bottom-4 left-1/2 z-50 flex translate-x-[calc(215px-100%)] flex-col items-center"
+      className="fixed bottom-1 left-1/2 z-50 flex translate-x-[calc(220px-100%)] flex-col items-center"
       onClick={() => router.push("/chatbot")}>
-      <div className="mb-1 rounded-md bg-white px-3 py-1 text-sm shadow-md">
+      <div className="mb-[-2rem] rounded-full bg-white px-4 py-1.5 text-sm font-medium whitespace-nowrap shadow-md">
         무너에게 물어봐
       </div>
-      <div className="h-16 w-16">
+      <div className="h-40 w-40">
         <Canvas camera={{ position: [0, 0, 2.5] }}>
           <ambientLight intensity={0.7} />
+          <directionalLight position={[0, 0, 2]} intensity={0.6} />
+          <hemisphereLight intensity={0.6} groundColor="white" />
           <CharacterModel
             onClick={() => router.push("/chatbot")}
             isSpeaking={false}
