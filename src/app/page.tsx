5bc2345aac0b5116df24f548fe7edc6ca17bdac2
silverain <ekdldkaa@naver.com>
2025-06-15 13:57:10 +0900

    #123 chore: websocket테스팅용 page (삭제 예정)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useBleScanner } from "@/hooks/useBleScanner";
+import { useNearbySocket } from "@/hooks/useNearbySocket";
+import { useState } from "react";
+
 export default function Home() {
+  const [logs, setLogs] = useState<any[]>([]);
+  const wsRef = useNearbySocket((data) => {
+    setLogs((prev) => [...prev, data]);
+  });
+  useBleScanner(wsRef.current); //WebSocket에 BLE 거리 전송
   return (
     <div className="h-screen w-full">
-      <main></main>
+      <main>
+        <div className="p-4">
+          <h1 className="text-xl font-bold">Nearby User Logs</h1>
+          <ul className="mt-4">
+            {logs.map((log, idx) => (
+              <li key={idx}>
+                🛰️ {log.userId} | 거리: {log.distance}m
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
