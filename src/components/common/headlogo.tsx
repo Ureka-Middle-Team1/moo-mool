c3e2facb647afc0437650506b89c75da1f7afd2e
arty0928 <arty0928@konkuk.ac.kr>
2025-06-17 20:38:13 +0900

    #173 refactor: 무물 로고 컴포넌트화

---
+++ src/components/common/headlogo.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function HeadLogo() {
+  const router = useRouter();
+  const handleToHome = () => {
+    router.push("/home");
+  };
+
+  return (
+    <div className="flex cursor-pointer items-center justify-between">
+      {/* 좌측 로고 */}
+      <img
+        src="/assets/icons/logo.png"
+        alt="logo"
+        className="h-auto w-18"
+        onClick={handleToHome}
+      />
+    </div>
+  );
+}
