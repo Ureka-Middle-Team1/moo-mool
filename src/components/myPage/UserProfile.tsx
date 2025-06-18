669919127dff13f0fbb0591f35c52a7acb59c2d4
ahnminji <ahn_minji@naver.com>
2025-06-18 23:42:34 +0900

    #166 feat: 마이페이지 - 유저 프로필 불러올 때 공유카운트에 따른 level 이미지 불러오기

--- src/components/myPage/UserProfile.tsx
+++ src/components/myPage/UserProfile.tsx
@@ -2,28 +2,36 @@ import { useGetUserCharacterProfile } from "@/hooks/useGetUserCharacterProfile";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { useSession } from "next-auth/react";
 
-// 유저 프로필
-export default function UserProfile() {
+interface UserProfileProps {
+  invitedCount: number;
+}
+
+export default function UserProfile({ invitedCount }: UserProfileProps) {
   const { data: session } = useSession();
   const isLoggedIn = !!session?.user;
   const userId = session?.user?.id;
 
   const { data: userCharacterProfile } = useGetUserCharacterProfile(
-    userId ?? ""
+    userId ? userId : undefined
   );
+
+  const level = invitedCount >= 10 ? 3 : invitedCount >= 5 ? 2 : 1;
+
+  const characterType = userCharacterProfile?.type?.toLowerCase();
+
+  const imagePath = characterType
+    ? level === 1
+      ? `/assets/moono/${characterType}-moono.png`
+      : `/assets/moono/lv${level}/${characterType}-moono.png`
+    : session?.user?.image;
 
   return (
     <>
       {isLoggedIn ? (
-        <div className="flex w-full flex-row items-center gap-4 px-4 py-6">
-          {/* 아바타 */}
-          <Avatar className="relative h-16 w-16 rounded-full bg-gray-400">
+        <div className="flex w-full flex-row items-center gap-7 px-4 py-2">
+          <Avatar className="relative h-[6rem] w-[6rem] rounded-full bg-gray-500 shadow-md">
             <AvatarImage
-              src={
-                userCharacterProfile?.type
-                  ? `/assets/moono/${userCharacterProfile.type.toLowerCase()}-moono.png`
-                  : session.user.image
-              }
+              src={imagePath}
               alt="user-avatar"
               className="absolute inset-0 h-full w-full object-contain"
             />
@@ -32,12 +40,13 @@ export default function UserProfile() {
             </AvatarFallback>
           </Avatar>
 
-          {/* 이름, 이메일 */}
           <div className="flex flex-col">
-            <p className="text-base font-semibold text-gray-900">
+            <p className="text-2xl font-semibold text-gray-900">
               {session.user.name}님
             </p>
-            <p className="text-sm text-gray-500">{session.user.email}</p>
+            <p className="text-sm text-gray-700 underline">
+              {session.user.email}
+            </p>
           </div>
         </div>
       ) : (
