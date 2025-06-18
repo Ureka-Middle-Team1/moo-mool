6e43657ed236fab49e23eb8169e618dc5ae00316
arty0928 <arty0928@konkuk.ac.kr>
2025-06-18 14:17:11 +0900

    #0 hotfix: 메타데이터 오류

--- src/app/meme-test/page.tsx
+++ src/app/meme-test/page.tsx
@@ -1,12 +1,9 @@
 import TestHomePage from "./TestHomePage";
 import { generateMemeMetadata } from "@/utils/generateMemeMetadata";
+import { Metadata } from "next";
 
-export function generateMetadata({
-  searchParams,
-}: {
-  searchParams?: { inviter?: string | string[] };
-}) {
-  return generateMemeMetadata(searchParams?.inviter);
+export function generateMetadata(): Metadata {
+  return generateMemeMetadata(); // inviter 없음
 }
 
 export default function Page() {
--- src/app/meme-test/result/[id]/metadata.tsx
+++ src/app/meme-test/result/[id]/metadata.tsx
@@ -1,14 +1,10 @@
 import { generateMemeMetadata } from "@/utils/generateMemeMetadata";
+import { Metadata } from "next";
 
 type Props = {
-  params: { id: string };
-  searchParams: { inviter?: string };
+  searchParams?: { inviter?: string | string[] };
 };
 
-export function generateMetadata({
-  searchParams,
-}: {
-  searchParams?: { inviter?: string | string[] };
-}) {
+export function generateMetadata({ searchParams }: Props): Metadata {
   return generateMemeMetadata(searchParams?.inviter);
 }
