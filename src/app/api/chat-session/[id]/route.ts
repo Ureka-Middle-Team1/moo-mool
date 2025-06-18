f9c67cef66481c37042499dced1eb2826e982852
silverain <ekdldkaa@naver.com>
2025-06-18 22:03:48 +0900

    #0 hotfix: params형식변경

--- src/app/api/chat-session/[id]/route.ts
+++ src/app/api/chat-session/[id]/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(
   req: NextRequest,
-  params: Promise<{ id: string }>
+  { params }: { params: Promise<{ id: string }> }
 ): Promise<Response> {
   const { id } = await params;
   const sessionId = parseInt(id, 10);
