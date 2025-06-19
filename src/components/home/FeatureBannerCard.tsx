b366ea48662fe42a86632431f9467cbc5ce89c7f
silverain <ekdldkaa@naver.com>
2025-06-19 15:27:14 +0900

    #223 style: 배너 내 사진 위치 미세 조정

--- src/components/home/FeatureBannerCard.tsx
+++ src/components/home/FeatureBannerCard.tsx
@@ -13,7 +13,7 @@ export default function FeatureBannerCard({
 }: FeatureBannerCardProps) {
   return (
     <div className="w-full">
-      <div className="relative flex w-full flex-col justify-between overflow-hidden rounded-2xl bg-gradient-to-br from-[#E0FFE8] via-[#F0FAFF] to-[#FFFCEB] px-5 py-5 shadow-md sm:flex-row sm:items-center">
+      <div className="relative flex w-full flex-col justify-between overflow-hidden rounded-2xl bg-gradient-to-br from-[#E0FFE8] via-[#F0FAFF] to-[#FFFCEB] px-8 py-5 shadow-md sm:flex-row sm:items-center">
         {/* 텍스트 영역 */}
         <div className="z-10 max-w-[60%]">
           <p className="text-xs text-gray-600">{subtitle}</p>
@@ -24,7 +24,7 @@ export default function FeatureBannerCard({
         </div>
 
         {/* 이미지 영역 */}
-        <div className="absolute right-4 bottom-0 w-24 sm:static sm:w-32">
+        <div className="absolute right-8 bottom-5 w-24 sm:static">
           <img
             src={image}
             alt="기능 이미지"
