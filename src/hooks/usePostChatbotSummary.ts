6d2a3a0bf0d7db3ac9d0e6db223c8cd19fe67711
gichulLimitLess <hanascreen@konkuk.ac.kr>
2025-06-19 16:22:03 +0900

    #184 feat: 챗봇 대화 내역에 요금제 이름 정보 연동 및 ChatSession DB 수정

--- src/hooks/usePostChatbotSummary.ts
+++ src/hooks/usePostChatbotSummary.ts
@@ -7,6 +7,7 @@ import { useChatStore } from "@/store/useChatStore";
 type ChatbotSummaryInput = {
   userId: string;
   messages: Message[]; // 자유 형식
+  planId: number;
 };
 
 // 채팅 다 끝내고, 추천까지 받았을 때, 해당 Hook을 사용해 요약 진행
@@ -15,7 +16,7 @@ export const usePostChatbotSummary = () => {
   const { getLastBotMessage } = useChatStore.getState(); // 리렌더링 필요 X, 대화 내역 마지막 질문만 받아오면 됨, 그리고 그것은 요금제 추천 정보임
 
   return useMutation({
-    mutationFn: async ({ messages }: ChatbotSummaryInput) => {
+    mutationFn: async ({ messages, userId, planId }: ChatbotSummaryInput) => {
       const res = await client.post(
         "/chat-summary",
         {
@@ -27,14 +28,15 @@ export const usePostChatbotSummary = () => {
           },
         }
       );
-      return res.data;
-    },
-    onSuccess: (data, variables) => {
-      const summary = data.normalizedValue;
-      const planId = getLastBotMessage()?.planData?.id; // 대화를 끝냈다면, 맨 마지막 "bot" 대화는 planData를 무조건 포함하고 있음
-      const { userId, messages } = variables;
-      console.log("채팅 요약 성공: ", data, userId);
+
+      const summary = res.data.normalizedValue;
+
       submitChatSummary({ userId, messages, summary, planId });
+
+      return { planId, summary };
+    },
+    onSuccess: (data) => {
+      console.log("채팅 요약 및 저장 성공: ", data.planId, data.summary);
     },
     onError: (error) => {
       console.error("채팅 요약 실패: ", error);
