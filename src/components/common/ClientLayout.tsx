6986642c2d22dddc215d773f4ad90058b2607175
kiki1004 <kikilove66@gmail.com>
2025-06-23 15:55:40 +0900

    #0 hotfix: 메인레이아웃에 있는 clientLayout 렌더링 오류 처리

--- src/components/common/ClientLayout.tsx
+++ src/components/common/ClientLayout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import LayoutWrapper from "@/components/common/LayoutWrapper";
 import { useChatRoomExitCleanup } from "@/hooks/useChatRoomExitCleanup";
@@ -8,11 +9,17 @@ export default function ClientLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [mounted, setMounted] = useState(false);
   const pathname = usePathname();
   const isSwagger = pathname?.startsWith("/swagger");
 
   useChatRoomExitCleanup(); // 채팅창에서 다른 페이지로 벗어날 때 초기화하는 로직 수행
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
   return isSwagger ? (
     <>{children}</>
   ) : (
