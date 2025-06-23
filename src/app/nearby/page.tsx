611238df4dc0edd275cf9d7d12cbedfed356190d
kiki1004 <kikilove66@gmail.com>
2025-06-23 14:17:29 +0900

    #291 refactor: 밈테스트 로딩 추가

--- src/app/nearby/page.tsx
+++ src/app/nearby/page.tsx
@@ -2,11 +2,12 @@
 
 import { signIn, useSession } from "next-auth/react";
 import NearbyContent from "./NearbyContent";
+import GlobalLoading from "../loading";
 
 export default function NearbyPage() {
   const { data: session, status } = useSession();
 
-  if (status === "loading") return <div>로딩 중...</div>;
+  if (status === "loading") return <GlobalLoading />;
   if (status === "unauthenticated") {
     signIn("kakao", { callbackUrl: "/nearby" });
     return null;
