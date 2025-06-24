c93f81f747a8eaa4c99974b50cf3758fd1189b3f
silverain <ekdldkaa@naver.com>
2025-06-24 18:07:19 +0900

    #0 hotfix: loading page 전환

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import ChatbotPage from "./ChatbotPage";
+import GlobalLoading from "../loading";
 
 export default function ChatPage() {
   return (
-    <Suspense fallback={<div>Loading chat...</div>}>
+    <Suspense fallback={<GlobalLoading />}>
       <ChatbotPage />
     </Suspense>
   );
